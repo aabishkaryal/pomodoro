b80d056dbfe4b8273c947761eeeb204f65a25b2b
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-08-19 22:53:54 +0545

    update: updated favicon and other icons and meta data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,31 @@ import { Timer } from "components/timer";
 import type { NextPage } from "next";
 import React from "react";
 
+import Head from "next/head";
+
 const Home: NextPage = () => {
 	return (
-		<Flex width="100%" minWidth="300px" flexDir="column" alignItems="center">
-			<Header />
-			<Timer />
-		</Flex>
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="theme-color" content="#000000" />
+				<meta
+					name="description"
+					content="Pomodoro timer website for better time management."
+				/>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+				<link rel="manifest" href="/manifest.json" />
+				<title>Pomodoro</title>
+			</Head>
+			<Flex width="100%" minWidth="300px" flexDir="column" alignItems="center">
+				<Header />
+				<Timer />
+			</Flex>
+		</>
 	);
 };
 
