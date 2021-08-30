2bd66ab12d5eb9a1291f96c1a357474b4bbcb13a
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-08-30 20:18:52 +0545

    bug: fix inconsistent types

--- src/app/timer.ts
+++ src/app/timer.ts
@@ -2,28 +2,28 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CONFIG, DEFAULT } from "app/constants";
 
 type TimerState = {
-	config: CONFIG;
-	totalTime: number;
+    config: CONFIG;
+    totalTime: number;
 };
 
 const initialState: TimerState = {
-	config: CONFIG.WORK,
-	totalTime: DEFAULT[CONFIG.WORK],
+    config: CONFIG.WORK,
+    totalTime: Number(DEFAULT[CONFIG.WORK]),
 };
 
 const timerSlice = createSlice({
-	name: "timer",
-	initialState,
-	reducers: {
-		setConfig(state, action: PayloadAction<CONFIG>) {
-			state.config = action.payload;
-			return state;
-		},
-		setTotalTime(state, action: PayloadAction<number>) {
-			state.totalTime = action.payload;
-			return state;
-		},
-	},
+    name: "timer",
+    initialState,
+    reducers: {
+        setConfig(state, action: PayloadAction<CONFIG>) {
+            state.config = action.payload;
+            return state;
+        },
+        setTotalTime(state, action: PayloadAction<number>) {
+            state.totalTime = action.payload;
+            return state;
+        },
+    },
 });
 
 export const { setConfig, setTotalTime } = timerSlice.actions;
